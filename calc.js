2e88d140bff1181e98f82ab0265417b7e44c8dcc
rrohit2901 <rrohit2901@gmail.com>
2020-01-24 04:58:04 +0530

    Basic implementation of calculator

---
+++ calc.js
@@ -0,0 +1,41 @@
+
+function get_ans(){
+	var element = document.getElementById("div1");
+	if(element.firstChild){
+		var child = document.getElementById("ans");
+		element.removeChild(child);
+	}
+	var x = document.getElementById("first").value;
+	var y = document.getElementById("second").value;
+	var oper = document.getElementById("drop"),value;
+	var ans=0;
+	console.log(x);
+	console.log(y);
+	if(isNaN(x) || isNaN(y)){
+		alert("Insert integers only!!!");
+		return;
+	}
+	var a=parseInt(x,10);
+	var b=parseInt(y,10);
+	if(oper.value=="+"){
+		ans = a+b;
+	}
+	else if(oper.value=="-"){
+		ans = x-y;
+	}
+	else if(oper.value=="*"){
+		ans = x*y;
+	}
+	else if(oper.value=="/"){
+		ans = x/y;
+		if(y==0){
+			alert("Can't divide number by 0!!!");
+			return;
+		}
+	}
+	var para = document.createElement("p");
+	var node = document.createTextNode(ans);
+	para.appendChild(node);
+	element.appendChild(para);
+	para.setAttribute("id","ans");
+}
